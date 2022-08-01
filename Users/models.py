c370ad506f931276e3e75621fa99c7b84820f7e5
ramesh-inexture <ramesh.inexture@gmail.com>
2022-08-01 11:53:41 +0530

    first commit

--- Users/models.py
+++ Users/models.py
@@ -43,4 +43,32 @@ class User(AbstractBaseUser):
     first_name = models.CharField(max_length=250)
     last_name = models.CharField(max_length=250)
     user_name = models.CharField(max_length=150, unique=True)
-    profile_picture = models.ImageField(default='')
+    profile_picture = models.ImageField(default='default.jpeg', upload_to='profile_picture')
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"{self.id} | {self.email}"
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
